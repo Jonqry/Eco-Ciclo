235498fce914092d0981634339a2107237a0cc07
Arthur <arthurms707@gmail.com>
2026-06-08 22:39:17 -0300

    Login

--- frontend/latest/src/app/login/page.js
+++ frontend/latest/src/app/login/page.js
@@ -30,7 +30,7 @@ export default function LoginPage() {
     const API_URL = 'https://eco-ciclo-pfe-poo-aps-backend.onrender.com';
 
     try {
-        const response = await fetch(`${API_URL}/api/usuarios`, {
+        const response = await fetch(`${API_URL}/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, senha }),
